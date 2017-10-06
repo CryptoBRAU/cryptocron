8169d955c4c90a93d9cc6df0bdb1b2740264711a
Fagner Brack <github@fagnermartins.com>
2017-10-06 19:36:51 +1100

    Create engine to trigger transaction score

--- tests.spec.js
+++ tests.spec.js
@@ -1,27 +1,71 @@
-const chai = require('chai');
-const expect = chai.expect;
-
-it('Initializes the trading engine', () => {
-  const exchange = () => {};
-  const initializeEngine = () => {
+describe('Engine', () => {
+  const exchangeWithPushModelStub = () => {
+    let subscribedCallback = () => {};
     return {
-      register: (callback) => {
-        callback({ buy: 0.0007, sell: 0.0008 });
+      subscribe: (callback) => {
+        subscribedCallback = callback;
       },
-      start: () => {}
+      pushTransactionFromExchange: (value) => {
+        subscribedCallback(value);
+      }
     };
   };
 
-  const engine = initializeEngine(exchange('BTC_USDT'));
-  engine.register(function createActionScoreFor(transaction) {
-    expect(transaction).to.eql({
-      buy: 0.0007,
-      sell: 0.0008
-    });
+  const initializeEngine = (exchange) => {
+    let registeredTxScoreCreators = [];
     return {
-      buy: -1,
-      sell: -1
+      register: (txScoreCreator) => {
+        registeredTxScoreCreators.push(txScoreCreator);
+        return function unregister() {
+          for (let i = 0; i < registeredTxScoreCreators.length; i += 1) {
+            if (registeredTxScoreCreators[i] === txScoreCreator) {
+              registeredTxScoreCreators.splice(i, 1);
+              break;
+            }
+          }
+        };
+      },
+      start: () => {
+        exchange.subscribe((transaction) => {
+          registeredTxScoreCreators.forEach((txScoreCreator) => {
+            txScoreCreator(transaction);
+          });
+        });
+      }
     };
+  };
+
+  it('registers an action score to handle a transaction', () => {
+    expect.assertions(1);
+    const exchange = exchangeWithPushModelStub();
+    const engine = initializeEngine(exchange);
+    engine.register(function createTxScoreFor(transaction) {
+      expect(transaction).toEqual({ buy: 0.0007, sell: 0.0008 });
+      return { buy: -1, sell: -1 };
+    });
+    engine.start();
+    exchange.pushTransactionFromExchange({ buy: 0.0007, sell: 0.0008 });
   });
-  engine.start();
+
+  it('registers many action scores to handle multiple transactions', () => {
+    expect.assertions(2);
+    const exchange = exchangeWithPushModelStub();
+    const engine = initializeEngine(exchange);
+    engine.start();
+
+    const unregisterFirstAction = engine.register(function createFirstTxScoreFor(transaction) {
+      expect(transaction).toEqual({ buy: 0.0007, sell: 0.0008 });
+      return { buy: -1, sell: -1 };
+    });
+    exchange.pushTransactionFromExchange({ buy: 0.0007, sell: 0.0008 });
+    unregisterFirstAction();
+
+    const unregisterSecondAction = engine.register(function createSecondTxScoreFor(transaction) {
+      expect(transaction).toEqual({ buy: 0.0001, sell: 0.0002 });
+      return { buy: -1, sell: -1 };
+    });
+    exchange.pushTransactionFromExchange({ buy: 0.0001, sell: 0.0002 });
+    unregisterSecondAction();
+  });
+
 });
